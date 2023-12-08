1e8b2f7afef2f35da9dd70bbafa9e9c963f4fc8e
Luis Araujo <luis_araujocaballero@hotmail.com>
2023-12-08 10:40:16 +0100

    day 07 part 2

--- src/main/java/com/adventofcode/day07/Card.java
+++ src/main/java/com/adventofcode/day07/Card.java
@@ -12,6 +12,10 @@ public class Card {
 
     List<Integer> handList;
 
+    List<Integer> jokerList;
+
+    Boolean isJoker = false;
+
     public Card(String hand, int bid) {
         this.hand = hand;
         this.bid = bid;
@@ -19,7 +23,14 @@ public class Card {
         this.handList = getHandList(hand);
     }
 
-    Map<String, Integer> map = new HashMap<String, Integer>() {
+    public Card(String hand, int bid, boolean type2) {
+        this.hand = hand;
+        this.bid = bid;
+        this.type = calculateCardType2(hand);
+        this.handList = getHandList(hand);
+    }
+
+    Map<String, Integer> map = new HashMap<>() {
         {
             put("A", 14);
             put("K", 13);
@@ -42,6 +53,19 @@ public class Card {
             try {
                 return Integer.parseInt(a);
             } catch (Exception e) {
+                return map.get(a);
+            }
+        }).toList();
+    }
+
+    private List<Integer> getJokerList(String hand) {
+        return Arrays.stream(hand.split("")).map(a -> {
+            try {
+                return Integer.parseInt(a);
+            } catch (Exception e) {
+                if (a.equals("J")) {
+                    return 1;
+                }
                 return map.get(a);
             }
         }).toList();
@@ -81,8 +105,96 @@ public class Card {
         return null;
     }
 
+    private CardType calculateCardType2(String hand) {
+        var array = hand.split("");
+        var set = new HashSet<>(List.of(array));
+        var setArray = new ArrayList<>(set);
+
+        if (set.size() == 1) {
+            if(set.contains("J")) {
+                this.isJoker = true;
+                this.jokerList = getJokerList(hand);
+            } else {
+                this.jokerList = getHandList(hand);
+            }
+            return CardType.FIVE_KIND;
+        } else if (set.size() == 2) {
+            var firstOccurrence = getOcurrences(hand, setArray.get(0).charAt(0));
+            var secondOccurrence = getOcurrences(hand, setArray.get(1).charAt(0));
+            var jokerOccurrences = getJokerOcurrences(hand);
+            if (jokerOccurrences == 0) {
+                this.jokerList = getHandList(hand);
+                if ((firstOccurrence == 1 && secondOccurrence == 4) || (firstOccurrence == 4 && secondOccurrence == 1)) {
+                    return CardType.FOUR_KIND;
+                } else {
+                    return CardType.FULL_HOUSE;
+                }
+            } else {
+                this.isJoker = true;
+                this.jokerList = getJokerList(hand);
+                return CardType.FIVE_KIND;
+            }
+
+        } else if (set.size() == 3) {
+            var firstOccurrence = getOcurrences(hand, setArray.get(0).charAt(0));
+            var secondOccurrence = getOcurrences(hand, setArray.get(1).charAt(0));
+            var thridOccurrence = getOcurrences(hand, setArray.get(2).charAt(0));
+            var jokerOccurrences = getJokerOcurrences(hand);
+
+            if (jokerOccurrences == 0) {
+                this.jokerList = getHandList(hand);
+
+                if (firstOccurrence == 3 || secondOccurrence == 3 || thridOccurrence == 3) {
+                    return CardType.THREE_KIND;
+                } else {
+                    return CardType.TWO_PAIR;
+                }
+            } else {
+                this.isJoker = true;
+                this.jokerList = getJokerList(hand);
+                if ((firstOccurrence == 1 && !setArray.get(0).equals("J")) || (secondOccurrence == 1 && !setArray.get(1).equals("J")) || (thridOccurrence == 1 && !setArray.get(2).equals("J"))) {
+
+                    return CardType.FOUR_KIND;
+                }
+                return CardType.FULL_HOUSE;
+            }
+        } else if (set.size() == 4) {
+            var jokerOccurrences = getJokerOcurrences(hand);
+            if (jokerOccurrences == 0) {
+                this.jokerList = getHandList(hand);
+                return CardType.ONE_PAIR;
+            } else {
+                this.isJoker = true;
+                this.jokerList = getJokerList(hand);
+                return CardType.THREE_KIND;
+            }
+
+        } else if (set.size() == 5) {
+            var jokerOccurrences = getJokerOcurrences(hand);
+
+            if (jokerOccurrences == 0) {
+                this.jokerList = getHandList(hand);
+                return CardType.HIGH_CARD;
+            } else {
+                this.isJoker = true;
+                this.jokerList = getJokerList(hand);
+                return CardType.ONE_PAIR;
+            }
+        }
+        return null;
+    }
+
+    private int getJokerIndex(String hand, HashSet<Integer> set) {
+        var toArray = new ArrayList<>(set);
+        return toArray.indexOf("J");
+    }
+
     private int getOcurrences(String array, char string) {
         return (int) array.chars().filter(a -> a == string).count();
+    }
+
+    private int getJokerOcurrences(String array) {
+        return (int) array.chars().filter(a -> a == 'J').count();
     }
 
     public void setHand(String hand) {
@@ -116,6 +228,8 @@ public class Card {
                 ", bid=" + bid +
                 ", type=" + type +
                 ", handList=" + handList +
+                ", jokerList=" + jokerList +
+                ", isJoker=" + isJoker +
                 '}';
     }
 }
--- src/main/java/com/adventofcode/day07/Day07.java
+++ src/main/java/com/adventofcode/day07/Day07.java
@@ -16,31 +16,19 @@ public class Day07 {
         BufferedReader br;
         String st;
         var cards = new ArrayList<Card>();
-        var map = new HashMap<String, Integer>() {
-            {
-                put("A", 14);
-                put("K", 13);
-                put("Q", 12);
-                put("J", 11);
-                put("T", 10);
-                put("9", 9);
-                put("8", 8);
-                put("7", 7);
-                put("6", 6);
-                put("5", 5);
-                put("4", 4);
-                put("3", 3);
-                put("2", 2);
-            }
-        };
+        var cards2 = new ArrayList<Card>();
         try {
             br = new BufferedReader(new FileReader(file));
 
             while ((st = br.readLine()) != null) {
                 var cardString = st.split(" ");
                 var card = new Card(cardString[0], Integer.parseInt(cardString[1]));
+                var card2 = new Card(cardString[0], Integer.parseInt(cardString[1]), true);
                 cards.add(card);
+                cards2.add(card2);
+
             }
+
             // System.out.println("RESULT " + cards);
 
 
@@ -56,17 +44,46 @@ public class Day07 {
             });
 
 
+            cards2.sort((a, b) -> {
+                if (a.type.getValue() == b.type.getValue() && !a.isJoker && !b.isJoker) {
+                    for (int i = 0; i < a.handList.size(); i++) {
+                        if (!Objects.equals(a.handList.get(i), b.handList.get(i))) {
+                            return a.handList.get(i) - b.handList.get(i);
+                        }
+                    }
+                } else if (a.type.getValue() == b.type.getValue()) {
+                    for (int i = 0; i < a.jokerList.size(); i++) {
+                        if (!Objects.equals(a.jokerList.get(i), b.jokerList.get(i))) {
+                            return a.jokerList.get(i) - b.jokerList.get(i);
+                        }
+                    }
+                }
+                return a.type.getValue() - b.type.getValue();
+            });
+
             var i = 0;
             var resultPart1 = 0;
-            var previousType = -1;
             for (var a : cards) {
                 i++;
                 resultPart1 += a.getBid() * i;
-                System.out.println(a);
+                //System.out.println(a);
             }
 
             System.out.println("RESULT PART 1: " + resultPart1);
 
+            var j = 0;
+            var resultPart2 = 0;
+            for (var a : cards2) {
+                j++;
+                resultPart2 += a.getBid() * j;
+                //System.out.println(a);
+            }
+
+            System.out.println("RESULT PART 2: " + resultPart2);
+
+            // 253254392 is too high
+            // 252868259 is too low
+            // 253253225 correct
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
